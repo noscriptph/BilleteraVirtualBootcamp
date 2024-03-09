206fdb8a281363477da070fb8d4de34b55f97357
victor <103396791+noscriptph@users.noreply.github.com>
2024-03-09 14:52:13 -0300

    actualizacion

--- src/Main.java
+++ src/Main.java
@@ -17,26 +17,35 @@ public class Main {
          */
 
 
-        // Esto ya es un obtejo
-        Cuenta cuenta1= new Cuenta();
-        Cuenta cuenta2= new Cuenta();
+        // Esto ya es un objeto
+        Cuenta cuenta= new Cuenta(); //Cuenta es un constructor
 
+
+
+
+/*
         //Llenando la clase de datos
         cuenta1.numeroCuenta = 123456;
         cuenta1.titularCuenta="Agustin Romero";
-        cuenta1.saldoCuenta = 1000000;
+        cuenta1.saldoCuenta = 1000000;/
+*/
+        cuenta.setNumeroCuenta(123456);
+        cuenta.setTitularCuenta("Alexis Rebolledo");
+        cuenta.setSaldoCuenta(50);
 
+
+        /*ejercicio 8-3-2024
+         Cuenta cuenta2= new Cuenta(346535, "alexis rebolledo", 77677867);
         //Llenando la clase de datos
         cuenta2.numeroCuenta = 123457;
         cuenta2.titularCuenta="Victor Paillacar";
         cuenta2.saldoCuenta = 300000;
-
-
-
-        cuenta1.mostrarInformacionCuentaCoriente();
-
-
-
         cuenta2.mostrarInformacionCuentaCoriente();
+        */
+
+        System.out.println("Nombre de la cuenta: "+cuenta.getTitularCuenta());
+        cuenta.mostrarInformacionCuentaCoriente();
+
+
     }
 }
--- src/cl/billeteraVirtualBootcamp/clases/Cuenta.java
+++ src/cl/billeteraVirtualBootcamp/clases/Cuenta.java
@@ -10,19 +10,49 @@ public class Cuenta {
 public, private, protected y son palabras reservadas
  */
 
-    public int numeroCuenta;
-    public String titularCuenta;
-    public double saldoCuenta;
+    private int numeroCuenta;
+    private String titularCuenta;
+    private double saldoCuenta;
 
     /*
     2)Constructor
      */
+public Cuenta(){
 
+}
+public Cuenta(int numCuenta, String titular, double saldo){
+    this.numeroCuenta= numCuenta;
+    this.titularCuenta= titular;
+    this.saldoCuenta=saldo;
+}
     /*
     3)Metodos de acceso
      */
 
-    /*
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getTitularCuenta() {
+        return titularCuenta;
+    }
+
+    public void setTitularCuenta(String titularCuenta) {
+        this.titularCuenta = titularCuenta;
+    }
+
+    public double getSaldoCuenta() {
+        return saldoCuenta;
+    }
+
+    public void setSaldoCuenta(double saldoCuenta) {
+        this.saldoCuenta = saldoCuenta;
+    }
+/*
     4)Metodos de comportamiento
      */
 
